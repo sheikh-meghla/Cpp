ee3c65c49098c64ce0b4defcb0cf0ec2d1d35179
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-26 16:58:46 +0600

    Refactor code structure for improved readability and maintainability

--- test.c
+++ test.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
+int main(){
 
-int main() {
-    int n;
-    scanf("%d",&n);
+    int x,y;
+    scanf("%d %d",&x,&y);
 
-    int arr[n];
-
-    for(int i = 0; i < n; i++) {
-        scanf("%d",&arr[i]);
-        
+    if (x||y)
+    {
+        printf("On\n");
     }
-
-    for(int i = 0; i < n; i++) {
-        printf("%d",arr[i]);
+    else{
+        printf("Off\n");
     }
     
+
     return 0;
 }
