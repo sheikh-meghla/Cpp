e909c337072d5986b83614096039ee894e58b68b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-26 17:14:04 +0600

    Refactor code structure for improved readability and maintainability

---
+++ Switch.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    switch (x)
+    {
+        case 1:
+            cout << "Meghla\n";
+        case 2:
+            cout << "Rafi\n";
+        case 3:
+            cout << "Nurani Tasnim\n";
+
+   
+    }
+    
+    return 0;
+    
+} 
