199aaa64d3d76085140c408fa1e099089f671b2a
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-26 17:22:39 +0600

    Add switch statement to classify characters as vowels or consonants
    - Implemented a switch statement in a new file `switch_brack.cpp` to read a character input and determine if it is a vowel or consonant.
    - Cases for vowels 'a', 'e', 'i', 'o', 'u' are handled, with a default case for consonants.

---
+++ switch_brack.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char x;
+    cin >> x;
+    switch (x)
+    {
+        case 'a':
+            cout << "Vowel\n";
+            break;
+        case 'e':
+            cout << "Vowel\n";
+            break;
+        case 'i':
+            cout << "Vowel\n";
+            break;
+        case 'o':
+            cout << "Vowel\n";
+            break;
+        case 'u':
+            cout << "Vowel\n";
+            break;
+
+        default:
+            cout << "Consonent\n";
+
+    }
+    
+    return 0;
+    
+} 
