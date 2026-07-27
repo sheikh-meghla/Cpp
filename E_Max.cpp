553c9471b206ef8f24f4496eddbe59937670dfef
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-27 21:25:22 +0600

    Add implementation for finding the maximum element in an array

---
+++ E_Max.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int max = INT_MIN; 
+
+    for (int i = 0; i < n; i++)
+    {
+        if(max < a[i]) {
+            max = a[i];
+        }
+    }
+    cout << max<<endl;
+
+
+    
+    
+    return 0;
+}
