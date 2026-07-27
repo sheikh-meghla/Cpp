8ae3fd945ae965588e078dc809bc774d091d2596
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-27 20:28:12 +0600

    Add implementation for character classification (capital, small, digit)

---
+++ M_Capital_or_Small_or_Digit.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char a;
+    cin >> a;
+    if(a >= 48 && a <= 57)
+    {
+        cout << "IS DIGIT\n";
+    }
+    else if(a >= 65 && a <= 90)
+    {
+        cout << "ALPHA\nIS CAPITAL";
+    }
+    else if(a >= 97 && a <= 122)
+    {
+        cout << "ALPHA\nIS SMALL";
+    }
+    else if(a >= 97 && a <= 122)
+    {
+        cout << "ALPHA\nIS SMALL";
+    }
+    return 0;
+} 
