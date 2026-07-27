0d786c439ce3da01a95ce1e44cb0b8209201d77d
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-27 19:17:06 +0600

    Refactor code structure for improved readability and maintainability

---
+++ Dynamic/Dynamic_Array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    
+    int * a = new int[6];
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        cout << a[i] << " ";
+    }
+    
+    return 0;
+    
+} 
+
