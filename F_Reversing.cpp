1419e785a3feab17b6d176f3d1c2789b7de5c18b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-27 20:56:04 +0600

    Add implementation for reversing an array in C++

---
+++ F_Reversing.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    int a[t];
+    for (int i = 0; i < t; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = t-1; i >= 0; i--)
+    {
+        cout << a[i]<<" ";
+    }
+    
+
+    
+    return 0;
+} 
