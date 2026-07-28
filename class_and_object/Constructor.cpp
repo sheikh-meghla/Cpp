873bd74a77b04b866117483741eef209056d575a
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-28 20:13:46 +0600

    Refactor code structure for improved readability and maintainability

---
+++ class_and_object/Constructor.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Student
+{
+    public:
+    int cls;
+    int roll;
+    double GPA;
+
+    Student(int c,int r,int g){
+        cls = c;
+        roll = r;
+        GPA = g;
+    }
+
+};
+
+int main() {
+
+    Student Meghla(10,1,5.00);
+    Student Rafi(12,1,5.00);
+
+    
+    
+    
+
+    cout << "Class = "<<Meghla.cls<< " \n"<<"Roll = "<< Meghla.roll<< " \n"<<"GPA = "<< Meghla.GPA<<endl;
+    cout << "Class = "<<Rafi.cls<< " \n"<<"Roll = "<< Rafi.roll<< " \n"<<"GPA = "<< Rafi.GPA<<endl;
+
+    return 0;
+}
