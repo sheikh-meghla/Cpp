8a0f9ed117595971aa8ac4030e54b4a5526926c2
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-29 20:46:14 +0600

    Refactor code structure for improved readability and maintainability

---
+++ class_and_object/ascending-sort-function.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+
+    sort(array,array + n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << array [i]<<" ";
+    }
+    
+    
+    
+    return 0;
+}
