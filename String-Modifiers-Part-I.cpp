bdf136199c069f0e8f56320a02b92750ea18a13d
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-30 21:29:54 +0600

    Refactor string concatenation to use append method in String Modifiers program

--- String-Modifiers-Part-I.cpp
+++ String-Modifiers-Part-I.cpp
@@ -6,7 +6,10 @@ int main() {
 
     string s,s2;
     cin >> s >> s2;
-    s += s2;
+    // s += s2;
+    s.append(s2);
+    s.push_back('!');
+    s.pop_back();
     cout << s << endl;
 
     return 0;
