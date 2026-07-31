24fda2e839623cde024e2613e5bc7b4eeab4a8ae
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-31 22:18:39 +0600

    Add initial implementation of String-input-with-spaces program

---
+++ String-input-with-spaces.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    string s;
+    cin.ignore();
+    getline(cin,s);
+    cout << n << endl;
+    cout << s << endl;
+    return 0;
+}
