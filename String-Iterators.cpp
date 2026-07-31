65274a34508398bcee275b9e0a8ec3f456ce919d
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-31 22:15:13 +0600

    Update string in String Iterators program to include full name

--- String-Iterators.cpp
+++ String-Iterators.cpp
@@ -4,13 +4,18 @@ using namespace std;
 
 int main() {
 
-    string s = "Meghla";
+    string s = "Meghla Sheikh";
     // for (int i = 0; i < s.size(); i++)
     // {
     //     cout << s[i]<<endl;
     // }
-    cout << *s.begin()<< endl;    
-    cout << *(s.end()-1) << endl;
+    // cout << *s.begin()<< endl;    
+    // cout << *(s.end()-1) << endl;
+    for (auto it = s.begin(); it < s.end(); it++)
+    {
+        cout << *it <<endl;
+    }
+    
 
     return 0;
 }
