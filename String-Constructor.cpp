9a3bf03c5369af97b4d326f8585ee2a2d0daa2e8
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-01 15:03:33 +0600

    Add initial implementation of String Constructor program

---
+++ String-Constructor.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    // string s = "Meghla Sheikh";
+    // string s("Meghla Sheikh",4);
+    // string t(s,4);
+    string s(5,'A');
+    cout << s;
+    return 0;
+}
