be312feabe75df0bc53b054ba486fad782c07e17
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-03 07:49:57 +0600

    Add initial implementation of H. N Times program

---
+++ H_N_Times.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        char ch;
+        cin >> n >> ch;
+        for (int i = 0; i < n; i++)
+        {
+            cout << ch<<" ";
+        }
+        cout << endl;
+        
+    }
+    
+    return 0;
+}
